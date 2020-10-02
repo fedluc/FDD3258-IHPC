999f7e6dfdc79ba8d18a315dfd1da531824b87ba
fedluc <71817492+fedluc@users.noreply.github.com>
2020-10-02 12:39:27 +0200

    Add files via upload

---
+++ home_assignment_3/exercise_2/non_blocking_linear/pi.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+#include <math.h>
+#include <time.h>
+
+int pi_mpi(int argc, char *argv[], int flip, double *out);
+int pi_local(int flip, int rank);
+
+	      
+int main(int argc, char *argv[]) {
+
+  int local_count, ii, flip = 1 << 24;
+  int rank, num_ranks, provided;
+  double pi;
+  
+  // Initialize MPI
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+
+  // Start timing
+  double start_time, end_time;
+  start_time = MPI_Wtime();
+  
+  // Info about processes and local rank
+  MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  // Root process
+  if (rank == 0){
+
+    int counts[num_ranks - 1];
+    MPI_Request requests[num_ranks - 1];
+    int global_count;
+
+    // Begin communication wit other processes
+    for (ii = 1; ii < num_ranks; ii++){
+      MPI_Irecv(&counts[ii - 1], 1, MPI_INT, ii,
+	       0, MPI_COMM_WORLD, &requests[ii - 1]);
+    }
+
+    // Update local count
+    local_count = pi_local(flip/num_ranks, rank);
+
+    // Ensure that the communication is completed
+    MPI_Waitall(num_ranks - 1, requests, MPI_STATUSES_IGNORE);
+
+    // Update global count
+    global_count = local_count;
+    for (ii = 0; ii < num_ranks - 1; ii++){
+      global_count += counts[ii];
+    }
+
+    // Compute pi
+    pi = ((double) global_count / (double) flip) * 4.0;
+    
+  }
+  // All other processes
+  else{
+
+    // Update local count
+    local_count = pi_local(flip/num_ranks, rank);
+    
+    // Send data to root
+    MPI_Send(&local_count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+  }
+
+  // End timing
+  end_time = MPI_Wtime();
+
+  // Output
+  if (rank == 0) {
+    printf("--------------------\nNumber of MPI ranks: %d\n",num_ranks);
+    printf("pi = %.8e\n", pi);
+    printf("Elapsed time : %.8e\n", end_time - start_time);
+  }
+  
+  // Finalize MPI
+  MPI_Finalize();
+
+  return 0;
+ 
+  
+}
+
+
+int pi_local(int flip, int rank) {
+
+  int ii, count=0;
+  double xx, yy, zz;
+ 
+  // Initialize seed of random number generator
+  srand(time(NULL) + 123456789 + rank*100);
+
+  // Monte-Carlo calculation of pi
+  for (ii = 0; ii < flip; ii++){
+
+    // Random (x,y) cartesian coordinates
+    xx = (double)random() / (double)RAND_MAX;
+    yy = (double)random() / (double)RAND_MAX;
+
+    // Radial distance
+    zz = sqrt(xx*xx + yy*yy);
+
+    // Update count if the radial distance falls inside the circle of unit radius
+    if (zz <= 1.0){
+      count++;
+    }
+  
+  }
+
+  return count;
+    
+}
